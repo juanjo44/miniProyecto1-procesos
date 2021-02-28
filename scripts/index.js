7a2592ea9c01f180f5d17124208372b02cd57e3c
NicolasDelgado1228 <nsdr1228@gmail.com>
2021-02-28 13:06:32 -0500

    Se añade la funcionalidad sacar/meter de la lista de los más recientes, tanto monedas como billetes

--- scripts/index.js
+++ scripts/index.js
@@ -38,3 +38,79 @@ const searchCoins = (africa, america, antartica, asia, europe, oceania) => {
 const searchBills = (africa, america, antartica, asia, europe, oceania) => {
     console.log("Los 3 billetes con más likes");
 };
+
+//order by time asc
+db.collection("americaBills").orderBy("created", "desc").limitToFirst(1).get().then((snapshot) => {
+    snapshot.docs.forEach(docs => {
+        console.log(docs);
+    });
+});
+
+
+// Actualizar los billetes más recientes
+const updateRecentBills = (nuevo) => {
+
+    let ultimo = db.collection("recentBills").orderBy("created", "asc").limit(1).get();
+
+    //Se borra el más antiguo (primero en asc)
+    db.collection("recentBills").doc(ultimo.id).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    
+    //Se añade el que se acaba de crear
+    db.collection("recentBills").doc(nuevo.id).set({
+        ID : nuevo.data().ID,
+        back : nuevo.data().back,
+        barter : nuevo.data().barter,
+        country : nuevo.data(). country,
+        created : nuevo.data().created,
+        denomsymbol : nuevo.data().denomsymbol,
+        denomvalue : nuevo.data().denomvalue,
+        front : nuevo.data().front,
+        likes : nuevo.data().likes,
+        year : nuevo.data().year
+    })
+    .then(() => {
+        console.log("Document successfully written!");
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
+    });
+        
+};
+
+// Actualizar las monedas más recientes
+const updateRecentCoins = (nuevo) => {
+
+    let ultimo = db.collection("recentCoins").orderBy("created", "asc").limit(1).get();
+
+    //Se borra el más antiguo (primero en asc)
+    db.collection("recentCoins").doc(ultimo.id).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    
+    //Se añade el que se acaba de crear
+    db.collection("recentCoins").doc(nuevo.id).set({
+        ID : nuevo.data().ID,
+        back : nuevo.data().back,
+        barter : nuevo.data().barter,
+        country : nuevo.data(). country,
+        created : nuevo.data().created,
+        denomsymbol : nuevo.data().denomsymbol,
+        denomvalue : nuevo.data().denomvalue,
+        front : nuevo.data().front,
+        likes : nuevo.data().likes,
+        year : nuevo.data().year
+    })
+    .then(() => {
+        console.log("Document successfully written!");
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
+    });
+        
+};
